c925fbfd30aae9cb52d2594439491c34ecc57118
Fahmim Mohammod Shahriar <mfahim.shahriar50@gmail.com>
2023-05-24 15:30:57 +0600

    first commit

---
+++ utlilty/dbconfig.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from rich import print as printc
+from rich.console import Console
+console = Console()
+def dbconfig():
+    try:
+        db = mysql.connector.connect( 
+            host = "localhost",
+            user = "pm"
+            password = "password"
+        )
+    except Exception as e:
+        print("[red][!] An error occurred while trying to connect database [/red]")
+        console.print_exception(show_locals=True)
+   
+    return db
